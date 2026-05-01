810f461a57d2339b631f5aaabbcef383de1096c8
NNS11 <nicolasnunes915@gmail.com>
2026-05-01 01:35:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ async function sendMessage() {
   const input = document.getElementById("input");
   const msg = input.value;
 
+  if (!msg) return;
+
   addMsg(msg, "user");
 
   const res = await fetch("http://localhost:8080/chat", {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({ msg })
   });
 
   const data = await res.json();
+
+  console.log(data);
 
   addMsg(data.output || "Erro na resposta", "bot");
 
