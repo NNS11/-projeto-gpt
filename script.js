8801a30e54194418d5a91352b0fa5a60dd9413e3
NNS11 <nicolasnunes915@gmail.com>
2026-05-01 01:32:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,18 @@
-const chatBox = document.getElementById("chat-box");
-
-function addMsg(text, type) {
-  const div = document.createElement("div");
-  div.className = "msg " + type;
-  div.innerText = text;
-  chatBox.appendChild(div);
-}
-
-function sendMessage() {
+async function sendMessage() {
   const input = document.getElementById("input");
-  const text = input.value;
+  const msg = input.value;
 
-  if (!text) return;
+  addMsg(msg, "user");
 
-  addMsg(text, "user");
-
-  setTimeout(() => {
-    addMsg("Resposta simulada (adicione IA depois)", "bot");
-  }, 500);
-
-  input.value = "";
-}
-
-async function generateImage() {
-  const input = document.getElementById("input");
-  const prompt = input.value;
-
-  if (!prompt) return;
-
-  addMsg("Gerando imagem...", "user");
-
-  const response = await fetch("http://localhost:8080/generate", {
+  const res = await fetch("http://localhost:8080/chat", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ prompt: prompt })
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({ msg })
   });
 
-  const data = await response.json();
+  const data = await res.json();
 
-  const img = document.createElement("img");
-  img.src = data.output[0];
-  img.style.maxWidth = "300px";
-  img.style.margin = "10px";
-
-  chatBox.appendChild(img);
+  addMsg(data.output || "Erro na resposta", "bot");
 
   input.value = "";
-}
-
-function clearChat() {
-  chatBox.innerHTML = "";
 }
